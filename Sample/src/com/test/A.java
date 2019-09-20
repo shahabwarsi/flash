0683134dae6d723868114885d9a2c80a4cc8d437
Ayyash <Ayyash@THE_RAIN>
2019-09-20 14:26:00 -0400

    second commit

---
+++ Sample/src/com/test/A.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class A
+{
+	public static void main(String[] args)
+	{
+		char[] a={'a','b'};
+		int [] b={1,9,0};
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
