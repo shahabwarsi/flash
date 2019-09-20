08e293a7a6cba082b64b4a03272e4d03fd9ee86a
Ayyash <Ayyash@THE_RAIN>
2019-09-20 14:17:30 -0400

    This is my first commit

---
+++ ourse-apia/src/main/java/com/test/CourseApiApp.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp
+{
+	public static void main(String[] args)
+	{
+		System.out.println("a");
+		SpringApplication.run(CourseApiApp.class, args);
+		
+	}
+
+}
